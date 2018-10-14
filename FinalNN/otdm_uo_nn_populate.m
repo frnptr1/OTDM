a52a5c4d942fec012b993a4fb3925704fd1f4b03
frnptr1 <piero.fronte@yahoo.it>
2018-10-14 21:08:40 +0200

    final working files

---
+++ FinalNN/otdm_uo_nn_populate.m
@@ -0,0 +1,42 @@
+function [xtr, ytr] = otdm_uo_nn_populate(p, rule)
+
+if rule ==1
+    xtr= round(rand(2,p));
+    ytr= zeros(p,1)';   
+    for j=1:p
+        if xtr(2,j) > xtr(1,j)
+        ytr(j) = 1;
+        end
+
+    end
+end
+
+
+if rule ==2
+    xtr= round(rand(2,p));
+    ytr= zeros(p,1)';
+    for j=1:p
+        if xtr(2,j) == xtr(1,j)
+            ytr(j) = 1;
+        end
+    end
+
+    xtr(xtr==0) = xtr(xtr==0)-1;
+
+end
+
+
+if rule ==21
+    xtr= round(rand(3,p));
+    ytr= zeros(p,1)';
+    for j=1:p
+        if xtr(2,j) == xtr(1,j)
+            ytr(j) = 1;
+            xtr(3,j) = xtr(2,j)*xtr(1,j);
+        end
+    end
+
+    xtr(xtr==0) = xtr(xtr==0)-1;
+
+end
+end
