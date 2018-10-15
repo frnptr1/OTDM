21177bc52232bbe6becef1b05ae35b917f1fbf4e
frnptr1 <31849298+frnptr1@users.noreply.github.com>
2018-10-15 18:50:11 +0200

    populate for n=3 fixed

--- FinalNN/otdm_uo_nn_populate.m
+++ FinalNN/otdm_uo_nn_populate.m
@@ -7,7 +7,6 @@ if rule ==1
         if xtr(2,j) > xtr(1,j)
         ytr(j) = 1;
         end
-
     end
 end
 
@@ -20,23 +19,20 @@ if rule ==2
             ytr(j) = 1;
         end
     end
-
     xtr(xtr==0) = xtr(xtr==0)-1;
-
 end
 
 
 if rule ==21
     xtr= round(rand(3,p));
     ytr= zeros(p,1)';
+    xtr(xtr==0) = xtr(xtr==0)-1;
     for j=1:p
         if xtr(2,j) == xtr(1,j)
-            ytr(j) = 1;
-            xtr(3,j) = xtr(2,j)*xtr(1,j);
+            ytr(j) = 1;            
         end
+        xtr(3,j) = xtr(2,j)*xtr(1,j);
     end
-
-    xtr(xtr==0) = xtr(xtr==0)-1;
-
 end
+
 end
